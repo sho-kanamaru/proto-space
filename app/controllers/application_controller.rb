e004e8d39acacccb141f42573db7d11269ca0470
金丸翔 <sho@sho-kanamaru.local>
2017-06-27 14:30:50 +0900

    define configure_permitted_parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,11 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path
   end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image, :group, :profile, :works])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image, :group, :profile, :works])
+  end
 end
