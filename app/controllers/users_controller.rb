37bf6f7e13062e158515a5fea10f3321445c4dea
金丸翔 <sho@sho-kanamaru.local>
2017-06-29 00:55:15 +0900

    define set_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,13 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to root_path
     else
@@ -19,5 +18,9 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :image, :email, :group, :profile, :works)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
