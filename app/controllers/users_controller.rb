099bc4c1d8529c02acfadad5ae78302bbcb0bf28
金丸翔 <sho@sho-kanamaru.local>
2017-06-29 00:35:21 +0900

    define update_action in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,18 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to root_path
+    else
+      render "edit"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :image, :email, :group, :profile, :works)
+  end
 end
