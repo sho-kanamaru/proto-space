315795d750924e2f6654519a206b3aebca370e3c
金丸翔 <sho@sho-kanamaru.local>
2017-06-29 00:34:17 +0900

    delete devise_parameter_sanitize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,5 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image, :group, :profile, :works])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image, :group, :profile, :works])
   end
 end
