a74c6e19ba3227c656b6120743d84f69fc2b655a
金丸翔 <sho@sho-kanamaru.local>
2017-06-30 22:09:33 +0900

    define reject_posts

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,5 +2,9 @@ class Prototype < ApplicationRecord
   validates :title, :catch_copy, :concept, presence: true
 
   has_many :captured_images
-  accepts_nested_attributes_for :captured_images, allow_destroy: true
+  accepts_nested_attributes_for :captured_images, allow_destroy: true, reject_if: :reject_posts
+
+  def reject_posts(attributed)
+    attributed['content'].blank?
+  end
 end
