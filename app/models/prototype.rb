cc5827d690b903d46f8d43c59bdb70b3f22f0f58
金丸翔 <sho@sho-kanamaru.local>
2017-07-01 00:39:47 +0900

    define get_main_content

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,10 +1,16 @@
 class Prototype < ApplicationRecord
   validates :title, :catch_copy, :concept, presence: true
 
+  belongs_to :user
+
   has_many :captured_images
   accepts_nested_attributes_for :captured_images, allow_destroy: true, reject_if: :reject_posts
 
   def reject_posts(attributed)
     attributed['content'].blank?
+  end
+
+  def get_main_content
+    captured_images.find_by(status: 0).content
   end
 end
