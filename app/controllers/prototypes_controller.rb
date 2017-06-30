ad3d87bccaff46a1ec8ec0e09b2c581b03272bc3
金丸翔 <sho@sho-kanamaru.local>
2017-06-30 21:15:09 +0900

    define create_action

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -6,5 +6,16 @@ class PrototypesController < ApplicationController
 
   def new
     @prototype = Prototype.new
+    @prototype.captured_images.build
   end
+
+  def create
+    Prototype.create(create_params)
+    redirect_to root_path
+  end
+
+  private
+    def create_params
+      params.require(:prototype).permit(:title, :catch_copy, :concept, captured_images_attributes: [:content, :status]).merge(user_id: current_user.id)
+    end
 end
