6c2b596f8f63bb8b0700536e94bb976bf6e4d52d
金丸翔 <sho@sho-kanamaru.local>
2017-07-01 00:39:02 +0900

    delete comment

--- app/models/captured_image.rb
+++ app/models/captured_image.rb
@@ -4,7 +4,4 @@ class CapturedImage < ApplicationRecord
   enum status: [:main, :sub]
 
   belongs_to :prototype, optional: true
-  # optional: trueをつけている理由は、railsの5系からbelongs_toの設定が変わり、DBに保存されるまでは、prototypeのidはnilなので、つまり、imageモデルでの時点では、prototype_id（imageレコードの外部キー）はnilなので、 required:trueのバリデーションに引っかかり、保存されなかったということになるらしい。
-  # ImageはProduct保存後でないと親レコードのIDが決まらないので保存できない。
-  # なので、prototypeのidがnilでもoptional: trueという設定で、imageを保存できるようにしてあげてます。
 end
