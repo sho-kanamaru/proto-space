0f39808db6b6eb3fd16bfd4aa2835a50994939d4
金丸翔 <sho@sho-kanamaru.local>
2017-06-30 15:29:42 +0900

    define new_action in prototypes_controller

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,10 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+
   def index
+  end
+
+  def new
+    @prototype = Prototype.new
   end
 end
