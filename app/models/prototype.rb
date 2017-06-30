0c97607ad84ff529e371ee4dddbc1e99672ee872
金丸翔 <sho@sho-kanamaru.local>
2017-06-30 21:31:51 +0900

    delete hash rocket

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,5 +2,5 @@ class Prototype < ApplicationRecord
   validates :title, :catch_copy, :concept, presence: true
 
   has_many :captured_images
-  accepts_nested_attributes_for :captured_images, :allow_destroy => true
+  accepts_nested_attributes_for :captured_images, allow_destroy: true
 end
