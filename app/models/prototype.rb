09b697393961af8185347b96d52587fd2fc1b128
金丸翔 <sho@sho-kanamaru.local>
2017-07-08 17:06:59 +0900

    define asosiation

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,6 +2,7 @@ class Prototype < ApplicationRecord
   validates :title, :catch_copy, :concept, presence: true
 
   belongs_to :user
+  has_many :likes, dependent: :destroy
 
   has_many :captured_images, dependent: :destroy
   accepts_nested_attributes_for :captured_images, allow_destroy: true, reject_if: :reject_posts
