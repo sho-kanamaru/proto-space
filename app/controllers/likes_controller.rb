2b84356b3fe733aea4324242d64f8f63d46ebafb
金丸翔 <sho@sho-kanamaru.local>
2017-07-08 17:25:22 +0900

    define destroy_action

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,4 +3,9 @@ class LikesController < ApplicationController
     @prototype = Prototype.find(params[:prototype_id])
     @like = @prototype.likes.create(user_id: current_user.id)
   end
+
+  def destroy
+    @prototype = Prototype.find(params[:prototype_id])
+    @prototype.likes.find_by(user_id: current_user.id).destroy
+  end
 end
