214fde12fafa3a376f6ad5fd5991e9dcbc88dd32
金丸翔 <sho@sho-kanamaru.local>
2017-07-11 13:02:28 +0900

    create comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170708080422) do
+ActiveRecord::Schema.define(version: 20170711040120) do
 
   create_table "captured_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content",      null: false
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20170708080422) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.index ["prototype_id"], name: "index_captured_images_on_prototype_id", using: :btree
+  end
+
+  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "content",      limit: 65535, null: false
+    t.integer  "prototype_id"
+    t.integer  "user_id"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.index ["prototype_id"], name: "index_comments_on_prototype_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
 
   create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -64,6 +74,8 @@ ActiveRecord::Schema.define(version: 20170708080422) do
   end
 
   add_foreign_key "captured_images", "prototypes"
+  add_foreign_key "comments", "prototypes"
+  add_foreign_key "comments", "users"
   add_foreign_key "likes", "prototypes"
   add_foreign_key "likes", "users"
   add_foreign_key "prototypes", "users"
