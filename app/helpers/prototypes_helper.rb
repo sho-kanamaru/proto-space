42d0e8512ba6bdf6c8adac1bbccbfd6242cbaaf0
金丸翔 <sho@sho-kanamaru.local>
2017-07-17 14:30:41 +0900

    create helper_method set_placeholder

--- app/helpers/prototypes_helper.rb
+++ app/helpers/prototypes_helper.rb
@@ -2,4 +2,8 @@ module PrototypesHelper
   def prototype_date(prototype)
     prototype.created_at.to_s(:date)
   end
+
+  def set_placeholder
+    placeholder = ["Web Design", "UI", "Application"]
+  end
 end
