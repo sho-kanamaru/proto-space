25b0c60ec3f39d15eaad0a00a897bef115608f1e
金丸翔 <sho@sho-kanamaru.local>
2017-07-17 11:40:27 +0900

    define show_action

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,4 +2,9 @@ class TagsController < ApplicationController
   def index
     @tags = ActsAsTaggableOn::Tag.most_used
   end
+
+  def show
+    @tag =  ActsAsTaggableOn::Tag.find(params[:id])
+    @prototypes = Prototype.tagged_with(@tag.name).page(params[:page]).like
+  end
 end
