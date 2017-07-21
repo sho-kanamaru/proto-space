16ad8911113b6d6f4d901de34e2fd4ae03a698b5
金丸翔 <sho@sho-kanamaru.local>
2017-07-21 14:11:47 +0900

    is valid with a password that has more than 8 characters

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,12 @@ describe User do
       it 'is valid with all information' do
         expect(user).to be_valid
       end
+
+      it "is valid with a password that has more than 8 characters" do
+        user.password = "00000000"
+        user.password_confirmation = "00000000"
+        expect(user).to be_valid
+      end
     end
 
     context 'without valid attributes' do
