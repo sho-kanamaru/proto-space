484ddba9d843741d1d95c208a7b9658064c4073f
金丸翔 <sho@sho-kanamaru.local>
2017-07-21 13:37:43 +0900

    is invalid without a name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,5 +11,13 @@ describe User do
         expect(user).to be_valid
       end
     end
+
+    context 'without valid attributes' do
+      it 'is invalid without a name' do
+        user.name = nil
+        user.valid?
+        expect(user.errors[:name]).to include("can't be blank")
+      end
+    end
   end
 end
