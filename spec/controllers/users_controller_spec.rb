ddb4dfc880a1215cd4b8a453c3e20177fda15d36
金丸翔 <sho@sho-kanamaru.local>
2017-07-27 16:59:16 +0900

    edit update_test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -44,34 +44,61 @@ describe UsersController, type: :controller do
     end
 
     describe 'PATCH #update' do
+      context 'with valid attributes' do
 
-      subject {
-        Proc.new { patch :update, params: { id: user, user: { name: "sho", email: "hoge@gmail.com", group: "techcamp", profile: "expert", works: "mentor" } } }
-      }
+        subject {
+          Proc.new { patch :update, params: { id: user, user: { name: "sho", email: "hoge@gmail.com", group: "techcamp", profile: "expert", works: "mentor" } } }
+        }
 
-      it 'assigns the requested to @user' do
-        subject.call
-        expect(assigns(:user)).to eq user
+        it 'assigns the requested to @user' do
+          subject.call
+          expect(assigns(:user)).to eq user
+        end
+
+        it "changes @user's attributes" do
+          subject.call
+          user.reload
+          expect(user.name).to eq("sho")
+          expect(user.email).to eq("hoge@gmail.com")
+          expect(user.group).to eq("techcamp")
+          expect(user.profile).to eq("expert")
+          expect(user.works).to eq("mentor")
+        end
+
+        it 'redirects root path' do
+          subject.call
+          expect(response).to redirect_to root_path
+        end
+
+        it 'shows flash messages to show update account successfully' do
+          subject.call
+          expect(flash[:notice]).to eq 'edited your account in successfully.'
+        end
       end
 
-      it "changes @user's attributes" do
-        subject.call
-        user.reload
-        expect(user.name).to eq("sho")
-        expect(user.email).to eq("hoge@gmail.com")
-        expect(user.group).to eq("techcamp")
-        expect(user.profile).to eq("expert")
-        expect(user.works).to eq("mentor")
-      end
+      context 'with invalid attributes' do
 
-      it 'redirects root path' do
-        subject.call
-        expect(response).to redirect_to root_path
-      end
+        before do
+          patch :update, params: { id: user, user: { name: "", email: "hoge@gmail.com", group: "techcamp", profile: "expert", works: "mentor" } }
+          @originalname = user.name
+        end
 
-      it 'sends flash messages' do
-        subject.call
-        expect(flash[:notice]).to eq 'edited your account in successfully.'
+        it 'assigns the requested to @user' do
+          expect(assigns(:user)).to eq user
+        end
+
+        it 'does not update the new account' do
+          user.reload
+          expect(user.name).to eq @originalname
+        end
+
+        it 'renders the :edit template' do
+          expect(response).to render_template :edit
+        end
+
+        it 'shows flash messages to show update account unsuccessfully' do
+          expect(flash[:alert]).to eq 'cannot updated your account.'
+        end
       end
     end
   end
