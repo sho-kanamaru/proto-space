af1187fc12e681b0bbab9fdf934be1d1d1c27d87
金丸翔 <sho@sho-kanamaru.local>
2017-07-31 22:05:21 +0900

    describe 'DELETE #destroy'

--- spec/controllers/prototypes_controller_spec.rb
+++ spec/controllers/prototypes_controller_spec.rb
@@ -251,5 +251,12 @@ describe PrototypesController, type: :controller do
         expect(response).to redirect_to new_user_session_path
       end
     end
+
+    describe 'DELETE #destroy' do
+      it 'redirects sign_in page' do
+        delete :destroy, params: { id: prototype }
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
   end
 end
